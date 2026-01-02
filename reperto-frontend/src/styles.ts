95a091462a9e49ddbedeb6733b7032f887c6e34f
Aravindhan <104446384+Aravindh-dev12@users.noreply.github.com>
2026-01-02 09:18:15 +0530

    Change string quotes from single to double in styles

--- reperto-frontend/src/styles.ts
+++ reperto-frontend/src/styles.ts
@@ -1,7 +1,7 @@
 export const Colors = {
-  background: '#FFFFFF',
-  lightPurple: '#F3E8FF',
-  purple: '#7C4DFF',
-  text: '#2E2E2E',
-  border: '#EDE7FB'
+  background: "#FFFFFF",
+  lightPurple: "#F3E8FF",
+  purple: "#7C4DFF",
+  text: "#2E2E2E",
+  border: "#EDE7FB",
 };
